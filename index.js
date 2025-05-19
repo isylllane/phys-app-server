9331a41c327865499f77b4cd0ab386df2d2d32fa
isylllane <Lutik301@gmail.com>
2025-05-19 21:38:00 +0300

    nado

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fileUplaods = require('express-fileupload');
 const cors = require('cors');
 
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
